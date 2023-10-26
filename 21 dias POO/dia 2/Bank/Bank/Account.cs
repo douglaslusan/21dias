6019995957a3bdd71413f56e0182b31d5572d5f4
Douglas Luiz dos Santos <87389167+douglaslusan@users.noreply.github.com>
2023-10-26 15:56:14 -0300

    Add files via upload
    Inicio aula dia 3

---
+++ 21 dias POO/dia 2/Bank/Bank/Account.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class Account
+    {
+        private double Ballance { get; set; }
+
+        public Account()
+        {
+        }
+
+        public Account(double inicialDeposit)
+        {
+            Ballance = inicialDeposit;
+        }
+
+        public void DepositFunds(double funds)
+        {
+            Ballance += funds;
+        }
+
+        public void WithdrawFunds(double funds)
+        {
+            if (funds > Ballance)
+            {
+                funds = Ballance;
+                Ballance -= funds;
+            }
+            else
+                Ballance -= funds;
+        }
+
+        public double GetBalance()
+        {
+            return Ballance;
+        }
+    }
+}
---
+++ 21 dias POO/dia 2/Bank/Bank/Program.cs
@@ -0,0 +1,17 @@
+﻿using Bank;
+using System.Security.Cryptography;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Account ac1 = new Account(1000.0);
+
+        Console.WriteLine(ac1.GetBalance());
+
+        ac1.DepositFunds(500);
+
+        Console.WriteLine(ac1.GetBalance());
+
+    }
+}
